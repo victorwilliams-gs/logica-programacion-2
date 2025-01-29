8d8616fc04eaf0d157ba686beaeb21525771d2b3
Alan <alan.williams.brito@gmail.com>
2025-01-29 11:09:03 -0300

    Entrega logica progra 2

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+const prompt = require("prompt-sync")();
+
+let celsius = prompt("Ingrese la temperatura (°C): ");
+
+try{
+    celsius = Number(celsius);
+}
+catch{
+    console.log("Entrada invalida!");
+}
+
+const kelvin = celsius + 273.15;
+const fahrenh = (celsius * 9/5) + 32;
+
+console.log("Grados Kelvin: ", kelvin);
+console.log("Grados Fahrenheit: ", fahrenh);
